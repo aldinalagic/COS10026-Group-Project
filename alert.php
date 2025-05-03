be83b36624dd4a5bacdc39347e5bd8bd8f5eab9b
aldinioh <aldin0505@gmail.com>
2025-05-03 19:42:34 +1000

    Added alert styling

--- alert.php
+++ alert.php
@@ -1,12 +1,21 @@
 <?php
-    enum AlertType {
-        case SUCCESS = "success";
-        case INFO = "info";
-        case WARNING = "warning";
-        case DANGER = "danger";
-    }
+// Unfortunately for some weird
 
-    function createAlert($message = "Alert message!", $type = AlertType::SUCCESS) {
-        return "<div class='alert' id='alert-$type'>$message</div>";
-    }
+
+// This enum defines the types of alerts that can be created.
+enum AlertType: string {
+    case SUCCESS = 'success';
+    case INFO = 'info';
+    case WARNING = 'warning';
+    case DANGER = 'danger';
+}
+
+
+// This function creates an alert message with a specified type and message.
+function createAlert(string $message = "Alert message!", AlertType $type = AlertType::SUCCESS) {
+    
+    
+    return "<div class='alert' id='alert-{$type->value}'>$message</div>";
+}
+
 ?>
